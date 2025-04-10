4f82f4176b7e5cfcc2106d969cde00163bd40997
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-04-10 00:37:00 -0400

    made tile controller reactive

--- src/routes/Tile.svelte.ts
+++ src/routes/Tile.svelte.ts
@@ -1,8 +1,11 @@
+import type { Player } from "./Player"
+
 export class Tile {
 	id: string
-	controller: string | null = null // id of the player who controls this tile, or null if no one does
 	terrain: TileTerrain = "land" // type of terrain on this tile
 	polygons: [number, number][][] = [] // an array of possibly multiple polygons. all are drawn together in one svg <path> element.
+	
+	controller = $state<Player | null>(null) // the player who controls this tile, or null if no one does
 
 	constructor(id: string, polygons: [number, number][][]) {
 		this.id = id
