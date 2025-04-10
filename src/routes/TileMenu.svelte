d1438863a43ebc879771bb0dd73910d97975812a
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-04-10 19:42:41 -0400

    add buttons to increase population & industry

--- src/routes/TileMenu.svelte
+++ src/routes/TileMenu.svelte
@@ -7,11 +7,30 @@
 <details open class="rounded-md bg-gray-100 p-4">
 	<summary class="select-none">{tile ?? "No Tile Selected"}</summary>
 	{#if tile}
-		<ul class="list-disc ml-8">
+		<ul class="ml-8 list-disc">
 			<li>Controller: {tile.controller}</li>
 			<li>Terrain: {tile.terrain}</li>
-			<li>{tile.population} residents</li>
-			<li>{tile.industry} industry</li>
 		</ul>
+		<div class="mt-2 flex gap-2">
+			<button
+				class="h-8 cursor-pointer rounded-md bg-gray-200 px-2 py-1"
+				onclick={() => (tile.population += 1)}
+			>
+				<span>{tile.population}</span>
+				<img
+					class="inline h-full"
+					src="/ui_icons/population.png"
+					alt="Population"
+					title="Population"
+				/>
+			</button>
+			<button
+				class="h-8 cursor-pointer rounded-md bg-gray-200 px-2 py-1"
+				onclick={() => (tile.industry += 1)}
+			>
+				<span>{tile.industry}</span>
+				<img class="inline h-full" src="/ui_icons/industry.png" alt="Industry" title="Industry" />
+			</button>
+		</div>
 	{/if}
 </details>
