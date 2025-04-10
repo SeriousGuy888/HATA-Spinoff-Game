1a6199cc43326cab325a08092034a8ad9b451c4c
billzonium trioxide <38299346+SeriousGuy888@users.noreply.github.com>
2025-04-10 16:11:14 -0400

    Update PlayerList.svelte

--- src/routes/PlayerList.svelte
+++ src/routes/PlayerList.svelte
@@ -2,33 +2,31 @@
 	import { gameState, setUserControlledPlayer } from "./state/game_state.svelte.ts"
 </script>
 
-<div class="rounded-md bg-gray-100 p-4">
-	<details open>
-		<summary class="mr-4 font-bold">Player List</summary>
-		<ul class="mt-2 flex flex-col gap-2">
-			{#each Object.values(gameState.players) as player}
-				<li
-					class="block rounded border-blue-600 bg-gray-200 p-2"
-					class:border-r-6={gameState.playerControlledByUser?.id === player.id}
-				>
-					<p class="font-bold">{player}</p>
-					{#if player.controlledCountry}
-						<p>Controls {player.controlledCountry}</p>
-					{/if}
-					{#if gameState.playerControlledByUser?.id === player.id}
-						<p>You</p>
-					{:else}
-						<button
-							class="cursor-pointer hover:underline"
-							onclick={() => {
-								setUserControlledPlayer(player.id)
-							}}
-						>
-							Take Control
-						</button>
-					{/if}
-				</li>
-			{/each}
-		</ul>
-	</details>
-</div>
+<details open class="rounded-md bg-gray-100 p-4">
+	<summary class="select-none">Player List</summary>
+	<ul class="mt-2 flex flex-col gap-2">
+		{#each Object.values(gameState.players) as player}
+			<li
+				class="block rounded border-blue-600 bg-gray-200 p-2"
+				class:border-r-6={gameState.playerControlledByUser?.id === player.id}
+			>
+				<p class="font-bold">{player}</p>
+				{#if player.controlledCountry}
+					<p>Controls {player.controlledCountry}</p>
+				{/if}
+				{#if gameState.playerControlledByUser?.id === player.id}
+					<p>You</p>
+				{:else}
+					<button
+						class="cursor-pointer hover:underline"
+						onclick={() => {
+							setUserControlledPlayer(player.id)
+						}}
+					>
+						Take Control
+					</button>
+				{/if}
+			</li>
+		{/each}
+	</ul>
+</details>
