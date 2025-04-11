b7b9a583c80e29ff525d8e47a7f25b76d7b4b6ae
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-04-10 22:37:39 -0400

    simplified player selector

--- src/routes/PlayerList.svelte
+++ src/routes/PlayerList.svelte
@@ -6,26 +6,20 @@
 	<summary class="select-none">Player List</summary>
 	<ul class="mt-2 flex flex-col gap-2">
 		{#each Object.values(gameState.players) as player}
-			<li
-				class="block rounded border-blue-600 bg-gray-200 p-2"
-				class:border-r-6={gameState.playerControlledByUser?.id === player.id}
-			>
-				<p class="font-bold">{player}</p>
-				{#if player.controlledCountry}
-					<p>Controls {player.controlledCountry}</p>
-				{/if}
-				{#if gameState.playerControlledByUser?.id === player.id}
-					<p>You</p>
-				{:else}
-					<button
-						class="cursor-pointer hover:underline"
-						onclick={() => {
-							setUserControlledPlayer(player.id)
-						}}
-					>
-						Take Control
-					</button>
-				{/if}
+			<li class="block">
+				<button
+					class="block w-full text-left cursor-pointer rounded border-blue-600 bg-gray-200 p-2"
+					class:border-r-6={gameState.playerControlledByUser?.id === player.id}
+					tabindex="0"
+					onclick={() => {
+						setUserControlledPlayer(player.id)
+					}}
+				>
+					<p class="font-bold">{player}</p>
+					{#if player.controlledCountry}
+						<p>{player.controlledCountry}</p>
+					{/if}
+				</button>
 			</li>
 		{/each}
 	</ul>
