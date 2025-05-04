9bcc492de602cd54feae1269fb97927d59489bf6
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-05-04 16:11:54 -0400

    Update Tile.ts

--- backend/entities/Tile.ts
+++ backend/entities/Tile.ts
@@ -26,10 +26,7 @@ export class Tile {
 	}
 
 	tick() {
-		if (this.controller) {
-			const revenue = this.industry * this.population * 0.01
-			this.controller.balance += revenue
-		}
+		// do nothing
 	}
 
 	/**
